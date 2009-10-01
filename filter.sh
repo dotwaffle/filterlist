1c8606aae4f52c3510e109880e896bdc7924acf6
Matthew Walster <matthew@walster.org>
2009-10-01 22:42:02 +0100

    Added in some command line options that makes it spit out suitable commands for pasting straight into the terminal on Juniper or Cisco routers.

--- filter.sh
+++ filter.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/bash
-# Generates a filter list for a Juniper router based on an input AS-SET or ASN.
+# Generates a filter list for a router based on an input AS-SET or ASN.
+
+FILTERNAME="filter"
+INC=10
+IP_LIST=""
+
+while [[ $1 = -* ]]; do
+	case "$1" in
+		-t|--type)
+			TYPE="$2"
+			shift 2
+			;;
+		-n|--name)
+			FILTERNAME="$2"
+			shift 2
+			;;
+		*)
+			echo "Error: Unknown option: $1" >&2
+			exit 1
+			;;
+	esac
+done
 
 IS_SET=`echo $1 | cut -c3 | grep -`
 
@@ -10,12 +31,22 @@ else
 	AS_LIST=$1
 fi
 
-IP_LIST=""
-
 for i in $AS_LIST
 do
 	IP_LIST+=`whois -h whois.radb.net -i origin $i | grep route: | cut -f 2 -d: | sed 's/ //g'`
 	IP_LIST+=`echo " "`
 done
 
-echo $IP_LIST
+for i in $IP_LIST
+do
+	if [ "$TYPE" == "juniper" ]
+	then
+		echo "set policy-options policy-statement $FILTERNAME from route-filter $i exact"
+	elif [ "$TYPE" == "cisco" ]
+	then
+		echo "ip prefix-list $FILTERNAME $INC permit $i"
+		let INC=INC+10
+	else
+		echo $i
+	fi
+done
