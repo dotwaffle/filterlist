37fe282e66f41fad3dce6eff4c2937a4a35ddaf8
Matthew Walster <matthew@walster.org>
2009-10-01 22:52:47 +0100

    Added in comments and usage line.

--- filter.sh
+++ filter.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 # Generates a filter list for a router based on an input AS-SET or ASN.
 
+# Define a usage statement
+usage()
+{
+	echo "$0: A filterlist generator"
+	echo "Usage: $0 [OPTS] AS-SET"
+	echo "    -t | --type [juniper|cisco]"
+	echo "    -n | --name [Filter Name]"
+}
+
+# Initialise some variables, to make it safe to use
 FILTERNAME="filter"
 INC=10
 IP_LIST=""
 
+# Parse the command line options
 while [[ $1 = -* ]]; do
 	case "$1" in
 		-t|--type)
@@ -15,15 +26,22 @@ while [[ $1 = -* ]]; do
 			FILTERNAME="$2"
 			shift 2
 			;;
+		-h|--help)
+			usage
+			exit 1
+			;;
 		*)
 			echo "Error: Unknown option: $1" >&2
+			usage
 			exit 1
 			;;
 	esac
 done
 
+# Do we have an AS-SET or an ASN?
 IS_SET=`echo $1 | cut -c3 | grep -`
 
+# If we've got an AS-SET, use the handy !i and ,1 commands on RADB
 if [[ "-" == "$IS_SET" ]]
 then
 	AS_LIST=`whois -h whois.radb.net \!i$1,1`
@@ -31,12 +49,14 @@ else
 	AS_LIST=$1
 fi
 
+# Find out which prefixes are contained within that AS number, from RADB
 for i in $AS_LIST
 do
 	IP_LIST+=`whois -h whois.radb.net -i origin $i | grep route: | cut -f 2 -d: | sed 's/ //g'`
 	IP_LIST+=`echo " "`
 done
 
+# Format the output nicely
 for i in $IP_LIST
 do
 	if [ "$TYPE" == "juniper" ]
